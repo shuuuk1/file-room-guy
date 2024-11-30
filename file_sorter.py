fdab3cf2b87032e3904fc137f6d35cdee5f672b8
shuuuk1 <131673394+shuuuk1@users.noreply.github.com>
2024-11-30 19:31:40 +0200

    Update file_sorter.py
    creating folders for each file category

--- file_sorter.py
+++ file_sorter.py
@@ -5,3 +5,6 @@ path = r"D:\projects\file_sorter\file-room-guy\junk"
 filename = os.listdir(path)
 categories = ['images', 'text files', 'archives', 'exe files', 'docx files', 'pdf files', 'xlsx files']
 
+for i in range(0, len(categories)):
+    if not os.path.exists(path + categories[i]):
+        os.makedirs(categories[i])
