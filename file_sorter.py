012fed1ca18539e964c40a67a0afd23902d438ee
shuuuk1 <131673394+shuuuk1@users.noreply.github.com>
2024-11-30 19:24:02 +0200

    Update file_sorter.py
    files categories

--- file_sorter.py
+++ file_sorter.py
@@ -1,2 +1,7 @@
 # File Room Guy
-# commit
+
+import os, shutil
+path = r"D:\projects\file_sorter\file-room-guy\junk"
+filename = os.listdir(path)
+categories = ['images', 'text files', 'archives', 'exe files', 'docx files', 'pdf files', 'xlsx files']
+
