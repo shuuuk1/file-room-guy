70f2b12aecb8e4213a691827d931efe38401b1b6
shuuuk1 <131673394+shuuuk1@users.noreply.github.com>
2024-11-30 20:09:59 +0200

    succesfully moved the images to the desired folder

--- file_sorter.py
+++ file_sorter.py
@@ -1,10 +1,16 @@
 # File Room Guy
 
 import os, shutil
-path = r"D:\projects\file_sorter\file-room-guy\junk"
+path = r"D:/projects/file_sorter/file-room-guy/junk/"
 filename = os.listdir(path)
 categories = ['images', 'text files', 'archives', 'exe files', 'docx files', 'pdf files', 'xlsx files']
 
 for i in range(0, len(categories)):
     if not os.path.exists(path + categories[i]):
-        os.makedirs(categories[i])
+        os.makedirs(path + categories[i])
+
+for file in filename:
+    if '.png' in file and not os.path.exists(path + "images/" + file):
+        shutil.move(path + file, path + "images/" + file)
+    elif '.jpg' in file and not os.path.exists(path + "images/" + file):
+        shutil.move(path + file, path + "images/" + file)
